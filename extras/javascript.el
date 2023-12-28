8dc3ef25d0aac1a167e88baac53e5b02e955249a
Charles Lowell <cowboyd@frontside.com>
2023-12-27 22:52:56 -0600

    clean up the javascript integration

--- extras/javascript.el
+++ extras/javascript.el
@@ -7,23 +7,6 @@
   :ensure t
   :hook (prog-mode . add-node-modules-path))
 
-;; (defun @cowboyd/find-nearest-dominating-file (files dir)
-;;   "Find which file is the nearest to the current"
-;;   (if (string= dir "/") nil
-;;     (let* ((contents (directory-files dir))
-;; 	  (file (seq-find (lambda (file) (seq-contains-p contents file)) files)))
-;;       (or
-;;        (when file (concat (file-name-as-directory dir) file))
-;;        (@cowboyd/find-nearest-dominating-file files (directory-file-name (file-name-directory dir)))))))
-
-;; (defun @cowboyd/deno-project (&optional startdir)
-;;   "Is the current file inside a deno (and not a node)project"
-;;   (let* ((dir (directory-file-name (or startdir (file-name-directory (buffer-file-name)))))
-;; 	(project-file (@cowboyd/find-nearest-dominating-file
-;; 		       (list "deno.json" "package.json")
-;; 		       dir)))
-;;     (when (string= "deno" (file-name-base project-file)) project-file)))
-
 (add-to-list 'auto-mode-alist '("\\.ts\\'"    . typescript-ts-mode))
 (add-to-list 'auto-mode-alist '("\\.tsx\\'"    . tsx-ts-mode))
 (add-to-list 'auto-mode-alist '("\\.js\\'"    . js-ts-mode))
@@ -44,7 +27,7 @@
        (assoc
         (file-name-nondirectory m)
         '(("package.json" "typescript-language-server" "--stdio")
-          ("deno.json" "deno" "lsp" :initializationOptions '(:enable t :lint t))))))))
+          ("deno.json" "deno" "lsp" "--unstable" :initializationOptions '(:enable t :lint t))))))))
 
 ;; (defun my/project-find-function (dir)
 ;;   (when-let ((f (and (boundp eglot-lsp-context) eglot-lsp-context (@cowboyd/find-js-marker))))
