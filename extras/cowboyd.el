d0f56ecf203b6859ecfffdff2df27faab9f558c5
Charles Lowell <cowboyd@frontside.com>
2024-12-18 09:12:21 -0600

    Add multiple cursors

--- extras/cowboyd.el
+++ extras/cowboyd.el
@@ -293,3 +293,15 @@ sit just below the threshold."
   :config
   ;; load default config
   (require 'smartparens-config))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Multiple Cursors. How did I live a year without them!
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(use-package multiple-cursors
+  :ensure t
+  :bind (("C-S-c C-S-c" . 'mc/edit-lines)
+	 ("C->" . 'mc/mark-next-like-this)
+	 ("C-<" . 'mc/mark-previous-like-this)
+	 ("C-c C-<" . 'mc/mark-all-like-this)))
